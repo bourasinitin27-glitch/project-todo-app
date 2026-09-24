ab1b411cc297d5d5aac6850b58855d6fa485d99f
bourasinitin27-glitch <bourasinitin27@gmail.com>
2026-09-24 16:29:42 +0000

    edit server.js file in backend for connecting with database

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-    .connect("mongodb://localhost:27017/todoapp")
+    .connect("mongodb://mongodb:27017/todoapp")
     .then(() => {
         console.log("MongoDB connected");
     })
