f9772f28b5b1cb6966908575a44459842d25cdfa
bourasinitin27-glitch <bourasinitin27@gmail.com>
2026-09-24 16:34:59 +0000

    edit frontend/src.app.js for backend to frontend connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "http://13.53.119.117:5000";
 
 function App() {
   const [todos, setTodos] = useState([]);
